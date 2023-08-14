703e7b9168658b6cb62ca3d5cff8d64749389aaa
RPINerd <rpinerd@gmail.com>
2023-08-13 20:34:30 -0400

    Set up main loop

--- forza_dash.py
+++ forza_dash.py
@@ -7,12 +7,7 @@ import json
 import socket
 import sys
 
-# Read in data types from file
-data_types = {}
-with open("data_formats.txt", "r") as f:
-    lines = f.read().split("\n")
-    for line in lines:
-        data_types[line.split()[1]] = line.split()[0]
+import carstat
 
 
 def get_data(data):
@@ -51,21 +46,133 @@ def get_data(data):
     return return_dict
 
 
-# Open a socket to recieve the telemetry data
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def open_socket(server_address, port) -> socket.socket:
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to the port
-server_address = ("localhost", 10000)
-sock.bind(server_address)
+    # Bind the socket to the port
+    sock.bind(server_address)
 
-# Recieve incoming data
-data, address = sock.recvfrom(4096)
+    return sock
 
-# Decode the data
-data = data.decode()
 
-# Convert the data to a dictionary
-data = json.loads(data)
+def main():
+    # Read in data types from file
+    data_types = {}
+    with open("data_formats.txt", "r") as f:
+        lines = f.read().split("\n")
+        for line in lines:
+            data_types[line.split()[1]] = line.split()[0]
 
-# Print the data
-print(data)
+    # Establish connection to the telemetry output
+    sock = open_socket("localhost", 10000)
+
+    # Primary loop
+    while True:
+        # Recieve incoming data
+        data, address = sock.recvfrom(1500)
+
+        # Convert recieved data to a dictionary
+        returned_data = get_data(data)
+
+        # Transfer select data to variables
+        currentEngineRpm = returned_data["CurrentEngineRpm"]
+        accelerationX = returned_data["AccelerationX"]
+        accelerationY = returned_data["AccelerationY"]
+        accelerationZ = returned_data["AccelerationZ"]
+        velocityX = returned_data["VelocityX"]
+        velocityY = returned_data["VelocityY"]
+        velocityZ = returned_data["VelocityZ"]
+        yaw = returned_data["Yaw"]
+        pitch = returned_data["Pitch"]
+        roll = returned_data["Roll"]
+        normalizedSuspensionTravelFL = returned_data[
+            "NormalizedSuspensionTravelFrontLeft"
+        ]
+        normalizedSuspensionTravelFR = returned_data[
+            "NormalizedSuspensionTravelFrontRight"
+        ]
+        normalizedSuspensionTravelRL = returned_data[
+            "NormalizedSuspensionTravelRearLeft"
+        ]
+        normalizedSuspensionTravelRR = returned_data[
+            "NormalizedSuspensionTravelRearRight"
+        ]
+        tireSlipRatioFrontLeft = returned_data["TireSlipRatioFrontLeft"]
+        tireSlipRatioFrontRight = returned_data["TireSlipRatioFrontRight"]
+        tireSlipRatioRearLeft = returned_data["TireSlipRatioRearLeft"]
+        tireSlipRatioRearRight = returned_data["TireSlipRatioRearRight"]
+        tireSlipAngleFrontLeft = returned_data["TireSlipAngleFrontLeft"]
+        tireSlipAngleFrontRight = returned_data["TireSlipAngleFrontRight"]
+        tireSlipAngleRearLeft = returned_data["TireSlipAngleRearLeft"]
+        tireSlipAngleRearRight = returned_data["TireSlipAngleRearRight"]
+        tireCombinedSlipFrontLeft = returned_data["TireCombinedSlipFrontLeft"]
+        tireCombinedSlipFrontRight = returned_data["TireCombinedSlipFrontRight"]
+        tireCombinedSlipRearLeft = returned_data["TireCombinedSlipRearLeft"]
+        tireCombinedSlipRearRight = returned_data["TireCombinedSlipRearRight"]
+        suspensionTravelMetersFrontLeft = returned_data[
+            "SuspensionTravelMetersFrontLeft"
+        ]
+        suspensionTravelMetersFrontRight = returned_data[
+            "SuspensionTravelMetersFrontRight"
+        ]
+        suspensionTravelMetersRearLeft = returned_data["SuspensionTravelMetersRearLeft"]
+        suspensionTravelMetersRearRight = returned_data[
+            "SuspensionTravelMetersRearRight"
+        ]
+        speed = returned_data["Speed"]
+        power = returned_data["Power"]
+        torque = returned_data["Torque"]
+        tireTempFrontLeft = returned_data["TireTempFrontLeft"]
+        tireTempFrontRight = returned_data["TireTempFrontRight"]
+        tireTempRearLeft = returned_data["TireTempRearLeft"]
+        tireTempRearRight = returned_data["TireTempRearRight"]
+        boost = returned_data["Boost"]
+        accel = returned_data["Accel"]
+        brake = returned_data["Brake"]
+        clutch = returned_data["Clutch"]
+        handBrake = returned_data["HandBrake"]
+        gear = returned_data["Gear"]
+        steer = returned_data["Steer"]
+
+        # Unused data (for now)
+        # raceOn = returned_data["IsRaceOn"]
+        # timestamp = returned_data["TimestampMS"]
+        # engineMaxRpm = returned_data["EngineMaxRpm"]
+        # engineIdleRpm = returned_data["EngineIdleRpm"]
+        # angularVelocityX = returned_data["AngularVelocityX"]
+        # angularVelocityY = returned_data["AngularVelocityY"]
+        # angularVelocityZ = returned_data["AngularVelocityZ"]
+        # wheelRotationSpeedFrontLeft = returned_data["WheelRotationSpeedFrontLeft"]
+        # wheelRotationSpeedFrontRight = returned_data["WheelRotationSpeedFrontRight"]
+        # wheelRotationSpeedRearLeft = returned_data["WheelRotationSpeedRearLeft"]
+        # wheelRotationSpeedRearRight = returned_data["WheelRotationSpeedRearRight"]
+        # wheelOnRumbleStripFrontLeft = returned_data["WheelOnRumbleStripFrontLeft"]
+        # wheelOnRumbleStripFrontRight = returned_data["WheelOnRumbleStripFrontRight"]
+        # wheelOnRumbleStripRearLeft = returned_data["WheelOnRumbleStripRearLeft"]
+        # wheelOnRumbleStripRearRight = returned_data["WheelOnRumbleStripRearRight"]
+        # wheelInPuddleDepthFrontLeft = returned_data["WheelInPuddleDepthFrontLeft"]
+        # wheelInPuddleDepthFrontRight = returned_data["WheelInPuddleDepthFrontRight"]
+        # wheelInPuddleDepthRearLeft = returned_data["WheelInPuddleDepthRearLeft"]
+        # wheelInPuddleDepthRearRight = returned_data["WheelInPuddleDepthRearRight"]
+        # surfaceRumbleFrontLeft = returned_data["SurfaceRumbleFrontLeft"]
+        # surfaceRumbleFrontRight = returned_data["SurfaceRumbleFrontRight"]
+        # surfaceRumbleRearLeft = returned_data["SurfaceRumbleRearLeft"]
+        # surfaceRumbleRearRight = returned_data["SurfaceRumbleRearRight"]
+        # carOrdinal = returned_data["CarOrdinal"]
+        # carClass = returned_data["CarClass"]
+        # carPerformanceIndex = returned_data["CarPerformanceIndex"]
+        # drivetrainType = returned_data["DrivetrainType"]
+        # numCylinders = returned_data["NumCylinders"]
+        # positionX = returned_data["PositionX"]
+        # positionY = returned_data["PositionY"]
+        # positionZ = returned_data["PositionZ"]
+        # distanceTraveled = returned_data["DistanceTraveled"]
+        # bestLap = returned_data["BestLap"]
+        # lastLap = returned_data["LastLap"]
+        # currentLap = returned_data["CurrentLap"]
+        # currentRaceTime = returned_data["CurrentRaceTime"]
+        # lapNumber = returned_data["LapNumber"]
+        # racePosition = returned_data["RacePosition"]
+        # normalizedDrivingLine = returned_data["NormalizedDrivingLine"]
+        # normalizedAIBrakeDifference = returned_data["NormalizedAIBrakeDifference"]
