1fec242171832fc81395d9754d544b1b4abb4ae6
RPINerd <RPINerd@gmail.com>
2023-08-18 09:29:43 -0400

    Migrate byte_sku back to main

--- forza_dash.py
+++ forza_dash.py
@@ -4,9 +4,21 @@
 #
 
 import socket
+import struct
 import sys
 
 import carstat
+
+# Assigning sizes in bytes to each variable type
+BYTESKU = {
+    "s32": 4,  # Signed 32bit int, 4 bytes of size
+    "u32": 4,  # Unsigned 32bit int
+    "f32": 4,  # Floating point 32bit
+    "u16": 2,  # Unsigned 16bit int
+    "u8": 1,  # Unsigned 8bit int
+    "s8": 1,  # Signed 8bit int
+    "hzn": 12,  # Unknown, 12 bytes of.. something
+}
 
 
 def get_data(data):
@@ -16,9 +28,9 @@ def get_data(data):
     passed_data = data
 
     # For each data type, get size and then collect
-    for key in data_types:
-        d_type = data_types[key]
-        size = byte_skus[d_type]
+    for key in data:
+        d_type = data[key]
+        size = BYTESKU[d_type]
         current = passed_data[:size]
 
         decoded = 0
