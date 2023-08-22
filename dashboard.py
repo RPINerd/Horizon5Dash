25e4bca46419cbfed8ff938e01dea8dbc665e722
RPINerd <rpinerd@gmail.com>
2023-08-22 09:38:47 -0400

    Testing non-animated plot

--- dashboard.py
+++ dashboard.py
@@ -11,21 +11,22 @@ def plot(telemetry) -> None:
 
     plt.style.use("fivethirtyeight")
 
-    x_vals = []
-    y_vals = []
+    x_vals = [telemetry[i].timestamp for i in range(len(telemetry))]
+    y_vals = [telemetry[i].rpm for i in range(len(telemetry))]
 
-    def animate(i):
-        x_vals.append(telemetry.timestamp)
-        y_vals.append(telemetry.rpm)
+    # def animate(i):
+    #     x_vals.append(telemetry.timestamp)
+    #     y_vals.append(telemetry.rpm)
 
-        plt.cla()
-        plt.plot(x_vals, y_vals)
+    #     plt.cla()
+    #     plt.plot(x_vals, y_vals)
 
-        plt.legend(loc="upper left")
-        plt.tight_layout()
+    #     plt.legend(loc="upper left")
+    #     plt.tight_layout()
 
-        return x_vals, y_vals
+    #     return x_vals, y_vals
 
-    ani = animation.FuncAnimation(plt.gcf(), animate, interval=20, frames=100)
-
+    # ani = animation.FuncAnimation(plt.gcf(), animate, interval=20, frames=100)
+    plt.plot(x_vals, y_vals)
+    plt.tight_layout()
     plt.show()
