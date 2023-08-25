f7c3c6df30345edcafea5a87a1eef92646449424
RPINerd <RPINerd@gmail.com>
2023-08-25 09:55:41 -0400

    Consolidated dummy dash

--- forza_dash.py
+++ forza_dash.py
@@ -4,9 +4,11 @@
 #
 
 import argparse
+import pickle
+from time import sleep
 
+import carstat
 import dashboard
-import dummy_dash
 import termdash
 import utils
 
@@ -60,6 +62,59 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def dryrun(window) -> None:
+    # Parse the previously saved telemetry data from the pickle file
+    with open("telemetry_log.pkl", "rb") as pkl:
+        pickle_list = pickle.load(pkl)
+
+    # Step through the pickle_list and plot the data as if it were live
+    telemetry_window = []
+    print("Speed | RPM | Gear")
+    for tele_point in pickle_list:
+        # cols = line.split("\t")
+        # returned_data = utils.parse_data(cols, data_types)
+        returned_data = pickle.loads(tele_point)
+        telemetry = carstat.telemetry(returned_data)
+
+        if len(telemetry_window) < window:
+            telemetry_window.append(telemetry)
+        else:
+            telemetry_window.pop(0)
+            telemetry_window.append(telemetry)
+
+        ui = dashboard.build_ui()
+        dashboard.update_ui(ui)
+        sleep(0.01)
+
+
+def dump(ip, port, data_types) -> None:
+    # Establish connection to the telemetry output
+    sock = utils.open_socket(str(ip), port)
+
+    pickle_list = []
+    i = 0
+    while True:
+        if i == 3000:
+            break
+
+        # Recieve incoming data
+        data, address = sock.recvfrom(1500)
+
+        # Convert recieved data to a dictionary
+        returned_data = utils.parse_data(data, data_types)
+
+        # Create a pickle of the returned data and append it to the pickle_list
+        pickle_list.append(pickle.dumps(returned_data))
+
+        i += 1
+        print(i, end="\r")
+        sleep(0.01)
+
+    # Once the loop is broken, write the pickle_list to a file
+    with open("telemetry_log.pkl", "wb") as pkl:
+        pickle.dump(pickle_list, pkl)
+
+
 def main(args, data_types) -> None:
     # Establish connection to the telemetry output
     sock = utils.open_socket(str(args.ip), 10000)
@@ -91,8 +146,8 @@ if __name__ == "__main__":
     data_types = utils.get_datatypes()
 
     if args.dryrun:
-        dummy_dash.main(args, data_types, "run")
+        dryrun(args.window)
     elif args.dump:
-        dummy_dash.main(args, "dump")
+        dump(args.ip, args.port, data_types)
     else:
         main(args, data_types)
